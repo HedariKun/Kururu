69a611c5ae284cb83dea6cdcd8d0435f0272660e
hedarikun <hedariKun@gmail.com>
2018-11-30 23:33:30 +0300

    editing push sh file

--- Push.sh
+++ Push.sh
@@ -6,6 +6,6 @@ echo enter the commit description
 
 read CommitDescription
 
-git commit -m CommitDescription
+git commit -m "$CommitDescription"
 
 git push https://gitlab.com/HedariKun/kururur.git master
